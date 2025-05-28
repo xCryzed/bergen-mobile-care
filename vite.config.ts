c3af82ce52a0469341a7cba316aa08f2c3ba6c7f
Nino Bergen <nino.bergen@icloud.com>
2025-05-28 12:37:59 +0200

    build: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/bergen-mobile-care/',
   server: {
     host: "::",
     port: 8080,
